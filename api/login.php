f7483e2da032439ee9f7420d6bb1f53fce734535
tynsoo <christeensolis2209@gmail.com>
2026-09-23 03:07:21 +0800

    Revert "Restyle the admin/staff sidebar nav and add a profile card"
    This reverts commit bcea960e67ae2f6fa1df6441cd1145503a5a2bd9.

--- api/login.php
+++ api/login.php
@@ -76,7 +76,6 @@ $sessionData = [
     'id' => (int) $user['id'],
     'role' => $user['role'],
     'username' => $user['username'],
-    'email' => $user['email'],
     'avatarUrl' => $user['avatar_data_url'],
 ];
 
